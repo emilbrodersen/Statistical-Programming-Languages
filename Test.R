450c98ddc5fc9ba59a915d722f61ebbf1b86133f
emilbrodersen@gmail.com <emilbrodersen@gmail.com>
2016-12-11 13:22:52 +0100

    Some text cleaning

--- Test.R
+++ Test.R
@@ -3,9 +3,13 @@ library(data.table)
 
 df <- fread("C:\\Users\\Emil\\Desktop\\Statistical Programming Languages\\Amazon Fine Foods\\amazon-fine-foods\\Reviews.csv")
 
+# Create a 0/1 dependent variable depending on helpfullness numerator and denominator
+df$Helpful <- ifelse(df$HelpfulnessNumerator/df$HelpfulnessDenominator>0.8, 1, 0)
+df$Helpful.pct <- df$HelpfulnessNumerator/df$HelpfulnessDenominator
+
 # Subset relevant columns of the dataset (Helpfullness numerator and denominator, 
 # Score and Text)
-df <- df[, c(5, 6, 7, 10)]
+#df <- df[, c(5, 6, 7, 10)]
 
 # Subset reviews with more then 10 helpful or not indications
 df <- df[df$HelpfulnessDenominator>10]
@@ -17,13 +21,17 @@ sum(is.na(df))
 df$Text <- tolower(df$Text)
 
 # Clean Text variable
-cleanFun <- function(htmlString) {
-  return(gsub("<.*?>", "", htmlString))
-}
-df$Text <- cleanFun(df$Text)
-
+df$Text <- gsub("<.*?>", "", df$Text)
 df$Text <- gsub("length::", "", df$Text)
-
 df$Text <- gsub("\\d+:\\d+", "", df$Text)
+df$Text = gsub("[[:punct:]]", "", df$Text)
+df$Text = gsub("[[:digit:]]", "", df$Text)
+df$Text = gsub("http\\w+", "", df$Text)
+df$Text = gsub("[ \t]{2,}", "", df$Text)
+df$Text = gsub("^\\s+|\\s+$", "", df$Text)
 
-sum(sum(df$HelpfulnessNumerator)/sum(df$HelpfulnessDenominator)
+# Count how many times there is identical UserID. Then how many identical texts.
+df$n <- as.numeric(ave(as.character(df$UserId), df$UserId, FUN = length))
+df$n2 <- as.numeric(ave(as.character(df$Text), df$Text, FUN = length))
+
+df.uniq <- unique(df, by = c(10))
